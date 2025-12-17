f5e705fb65fdba2cf8a6b569fd1653d57bf37b7b
Wade Boohar <boohar@usc.edu>
2025-12-17 13:30:50 -0800

    updates to path + PCA analysis

--- Quantcell/quantcell.py
+++ Quantcell/quantcell.py
@@ -5,6 +5,7 @@ import joblib
 import numpy as np
 from tqdm import tqdm
 import time
+import pandas as pd
 
 from codex_project import codex_project
 
@@ -12,6 +13,7 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import average_precision_score, precision_recall_curve
+
 
 
 
@@ -29,6 +31,7 @@ class quantcell_project:
         self.excluded_cell_types = ['Other']
         self.is_fitted = False
         self.relabeled = False
+        self.relabeled_labels = None
 
 
     def initialize(self, base_path, project_name):
@@ -156,6 +159,90 @@ class quantcell_project:
         
         self.clf.fit(self.X_train, self.y_train)
         self.is_fitted = True
+
+
+    def PCA_check(self, strategy='all', num_sample=500, cell_types=None, output_dir=None, save=False):
+        if strategy not in ['all', 'specific']:
+            raise ValueError("Strategy must be 'all' or 'specific'")
+        if self.relabeled:
+            raise ValueError("Cannot perform PCA check after relabeling")
+        if not self.is_fitted:
+            raise ValueError("Model not fitted yet")
+        if self.relabeled_labels is None:
+            raise ValueError("No relabeled labels found. Please relabel unannotated cells first.")
+
+        if strategy == 'specific' and self.codex_project._marker_combos is None:
+            try:
+                annot_strategy = self.codex.loc[:, 'annotation_strategy']
+                if len(annot_strategy.unique()) > 1:
+                    raise ValueError("Multiple annotation strategies found in codex. Please ensure only one annotation strategy is used for 'specific' PCA check.")
+                
+                self.codex_project.read_annotation_strategy(f'../{annot_strategy.unique()[0]}')
+            except:
+                raise ValueError("No annotation strategy found. Please load an annotation strategy for 'specific' PCA check.")
+
+        from sklearn.decomposition import PCA
+        import matplotlib.pyplot as plt
+
+        if cell_types == None:
+            cell_types = self.target_encoder.classes_.tolist()
+        
+        reducer = PCA()
+
+        conventional_mask = self.codex.cell_type != 'Other'
+        conventional_features = self.codex.loc[conventional_mask, self._filter_metadata().columns]
+        conventional_labels = self.codex.cell_type[conventional_mask]
+
+        unlabeled_features = self.codex.loc[~conventional_mask, self._filter_metadata().columns]
+
+
+        for _cell_type in cell_types:
+            relabeled_mask = np.array(self.relabeled_labels) == _cell_type
+            if sum(relabeled_mask) < num_sample:
+                print(f"Warning: Not enough cells of type {_cell_type} for PCA plot. Found {sum(relabeled_mask)} which is less than the requested {num_sample}.")
+                print("Using all available cells for this cell type.")
+
+            unlabeled_features_subset = unlabeled_features.loc[relabeled_mask, :].sample(num_sample, random_state=self.random_seed, replace=True)
+
+            conventional_mask = conventional_labels == _cell_type
+            conventional_features_subset = conventional_features.loc[conventional_mask, :].sample(num_sample, random_state=self.random_seed, replace=True)
+
+            other_features = pd.concat([unlabeled_features.loc[~relabeled_mask, :], conventional_features.loc[~conventional_mask, :]], axis=0)
+            other_features_subset = other_features.sample(num_sample, random_state=self.random_seed, replace=True)
+
+            combined_features = pd.concat([unlabeled_features_subset, conventional_features_subset, other_features_subset], axis=0)
+
+            if strategy == 'specific':
+                markers_of_interest = self.codex_project._marker_combos[_cell_type]
+                markers_of_interest = [marker[:-1] for marker in markers_of_interest]  # remove + or - at end
+                marker_cols = [x for x in combined_features.columns if any(marker in x for marker in markers_of_interest)]
+                combined_features = combined_features.loc[:, marker_cols]            
+
+            fig, ax = plt.subplots(figsize=(10, 8))
+
+            reduced_data = reducer.fit_transform(combined_features)
+            ax.scatter(reduced_data[:len(unlabeled_features_subset), 0], reduced_data[:len(unlabeled_features_subset), 1], label='QuantCell', alpha=0.5, color='blue', s=14)
+            ax.scatter(reduced_data[len(unlabeled_features_subset):len(unlabeled_features_subset)+len(conventional_features_subset), 0], reduced_data[len(unlabeled_features_subset):len(unlabeled_features_subset)+len(conventional_features_subset), 1], label='Conventional', alpha=0.5, color='red', s=14)
+            ax.scatter(reduced_data[len(unlabeled_features_subset)+len(conventional_features_subset):, 0], reduced_data[len(unlabeled_features_subset)+len(conventional_features_subset):, 1], label='Other', alpha=0.5, color='black', s=14)
+            ax.set_title(f'PCA Plot for Cell Type: {_cell_type} ({strategy} markers)', fontsize=24)
+            ax.set_xlabel(f'PC1 ({reducer.explained_variance_ratio_[0]*100:.2f}% variance)', fontsize=20)
+            ax.set_ylabel(f'PC2 ({reducer.explained_variance_ratio_[1]*100:.2f}% variance)', fontsize=20)
+            ax.tick_params(axis='both', which='major', labelsize=20)
+            
+            import matplotlib.patches as mpatches
+            blue_patch = mpatches.Patch(color='blue', label='QuantCell')
+            red_patch = mpatches.Patch(color='red', label='Conventional')
+            black_patch = mpatches.Patch(color='black', label='Other')
+            ax.legend(handles=[red_patch, blue_patch, black_patch], fontsize=16, frameon=False)
+
+            if save:
+                if output_dir is None:
+                    output_dir = f"{self.base_path}/{self.project_name}/PCA_plots"
+                os.makedirs(output_dir, exist_ok=True)
+                plt.savefig(f"{output_dir}/PCA_{_cell_type}_{strategy}.png")
+            
+
+
 
     def find_fdr_threshold(self, min_FDR=0, max_FDR=0.25, number_points=100):
         if not self.is_fitted:
